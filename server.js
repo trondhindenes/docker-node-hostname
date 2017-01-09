1332bcc7f5438cf7d904423de64140327d218b89
Trond Hindenes <trond@hindenes.com>
2017-01-09 10:12:37 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var os = require("os");
+var http_port = process.env.HTTP_PORT || '80';
+var hostname = os.hostname();
+const PORT=http_port
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end(hostname);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
